c8c350d8d192e733bae6889be3ff4e97dbecbebb
Connor Wilson <cnwilson29@gmail.com>
2024-03-22 05:37:18 +0000

    cam working

--- camera.py
+++ camera.py
@@ -6,18 +6,18 @@ import time
 class Camera:
     def __init__(self):
         self.camera = Picamera2()
-        self.preview_config = camera.create_preview_configuration()
-        self.capture_config = camera.create_still_configuration()
+        self.preview_config = self.camera.create_preview_configuration()
+        self.capture_config = self.camera.create_still_configuration()
         self.camera.configure(self.preview_config)
         self.camera.start()
         time.sleep(2)
 
-    def take_image(self):
-        self.camera.switch_mode_and_capture_file(self.capture_config, "test_full.jpg")
-        image = Image.open("test_full.jpg")
+    def take_image(self, path="image.jpg"):
+        self.camera.switch_mode_and_capture_file(self.capture_config, path)
+        image = Image.open(path)
         return image
 
 if __name__ == "__main__":
     camera = Camera()
     image = camera.take_image()
-    image.show()
+    # image.show()
